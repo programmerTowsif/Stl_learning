5eb16a83360179ee30c6dafa94f38769848c9dca
programmerTowsif <programmer.towsif@gmail.com>
2026-07-22 17:35:38 +0600

    include tuple for basic

---
+++ tuple.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ tuple<string , int ,string>person;
+ cin>>get<0>(person)>>get<1>(person)>>get<2>(person);
+ cout<<get<0>(person)<<" "<<get<1>(person)<<" "<<get<2>(person)<<endl;
+ 
+}
