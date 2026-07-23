c8f4bc4357faf94dead33e9a59e3c865f0cf78aa
programmerTowsif <programmer.towsif@gmail.com>
2026-07-23 14:36:57 +0600

    unique value print

--- vector/pracProb.cpp
+++ vector/pracProb.cpp
@@ -23,3 +23,29 @@ int main() {
   }
     
 }
+//unique value printting 
+#include <bits/stdc++.h>
+using namespace std;
+void printVec(vector<int>&v,int i){
+    if(i == v.size()) return;
+    cout<<v[i]<<" ";
+    printVec(v,i+1);
+}
+
+int main() {
+  vector<int>v;
+  for(int i =0;i<5;i++){
+      int x;
+      cin>>x;
+      v.push_back(x);
+  }
+  sort(v.begin(),v.end());
+  cout<<v[0]<<" ";
+  for(auto i=1;i<v.size();i++)
+  {
+      if(v[i] != v[i-1]) {
+          cout<<v[i] <<" ";
+      }
+  }
+    
+}
